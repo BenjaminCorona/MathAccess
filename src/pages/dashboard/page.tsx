52bed7008571fa45ae0cc9d460128fa58fa68974
BenjaminCorona <benja13lara@gmail.com>
2025-05-19 18:53:52 -0600

    cambios en dashboard

--- src/pages/dashboard/page.tsx
+++ src/pages/dashboard/page.tsx
@@ -356,7 +356,7 @@ export default function DashboardPage() {
                                     <FilePlus className="h-5 w-5" />
                                     <span>Crear Contenido</span>
                                 </Link>
-                                {/** 
+                                
                                 <Link
                                     to="/create-problem"
                                     className="flex items-center gap-3 rounded-lg px-3 py-2 hover:bg-accent transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-background focus:ring-ring"
@@ -364,7 +364,7 @@ export default function DashboardPage() {
                                     <FileEdit className="h-5 w-5" />
                                     <span>Crear Problema</span>
                                 </Link>
-                                */}
+                                
                             </>
                         )}
 
